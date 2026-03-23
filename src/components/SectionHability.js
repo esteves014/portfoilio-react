15fcce08c5e056d308f381f98082e193ccf80bd5
aluno <aluno@fateclins.local>
2026-03-23 11:35:12 -0300

    Feat(section-hability): criação de cards

--- src/components/SectionHability.js
+++ src/components/SectionHability.js
@@ -1,50 +1,7 @@
 import { forwardRef } from 'react';
+import habilitys from './../data/Habilitys.json'
 
 const SectionHability = forwardRef((props, ref) => {
-
-    const habilitys = [{
-        'acronym': 'HTML',
-        'name': 'HTML',
-        'description': 'Linguagem de marcação de Hipertexto.',
-        'backgroun': 'rgb(96, 0, 0)',
-        'color': 'rgb(182, 0, 0)'
-    }, {
-        'acronym': 'JS',
-        'name': 'JavaScript',
-        'description': 'Linguagem de programação interpretada de alto nível, essencial para o desenvolvimento web.',
-        'backgroun': 'rgb(96, 0, 0)',
-        'color': 'rgb(182, 0, 0)'
-    }, {
-        'acronym': 'CSS',
-        'name': 'CSS',
-        'description': 'Linguagem de estilo essencial utilizada para definir a aparência, layout e design de documentos HTML.',
-        'backgroun': 'rgb(96, 0, 0)',
-        'color': 'rgb(182, 0, 0)'
-    }, {
-        'acronym': 'RC',
-        'name': 'React',
-        'description': 'React é uma biblioteca de UI que cuida das manipulações diretas do DOM.',
-        'backgroun': 'rgb(96, 0, 0)',
-        'color': 'rgb(182, 0, 0)'
-    }, {
-        'acronym': 'LR',
-        'name': 'Laravel',
-        'description': 'LAravel é uma biblioteca de Backend que cuida das manipulações diretas de dados.',
-        'backgroun': 'rgb(96, 0, 0)',
-        'color': 'rgb(182, 0, 0)'
-    }, {
-        'acronym': 'BT',
-        'name': 'Bootstrap',
-        'description': 'LAravel é uma biblioteca de Backend que cuida das manipulações diretas de dados.',
-        'backgroun': 'rgb(96, 0, 0)',
-        'color': 'rgb(182, 0, 0)'
-    }, {
-        'acronym': 'VJ',
-        'name': 'Vue.js',
-        'description': 'LAravel é uma biblioteca de Backend que cuida das manipulações diretas de dados.',
-        'backgroun': 'rgb(96, 0, 0)',
-        'color': 'rgb(182, 0, 0)'
-    }];
 
     return (
         <section id="hability" ref={ref} className="container hidden">
@@ -52,16 +9,19 @@ const SectionHability = forwardRef((props, ref) => {
                 <h1>Habilitys</h1>
                 <div className="effect-gradient"></div>
             </div>
-            <div className='row'>
+            <div className='row g-5'>
 
                 {habilitys.map((hability, index) => (
                     <div key={index} className='col-lg-3 col-sm-12 col-md-4'>
-                        <div className='bg-html'><h1 className='title-html'>{hability.acronym}</h1></div>
-                        <h2>{hability.name}</h2>
-                        <p>{hability.description}</p>
+                        <div className="container-hability px-3 py-3 rounded">
+                            <h1 className='title-hability p-5 mb-3 rounded-top' style={{ color: hability.color, backgroundColor: hability.backgroun }}>{hability.acronym}</h1>
+                            <h2>{hability.name}</h2>
+                            <p>{hability.description}</p>
+                            <p className='level_hability px-3 text-center'>Nível de habilidade(1 a 10): {hability.level_hability}</p>
+                        </div>
                     </div>
                 ))}
-            </div>
+            </div >
         </section >
     )
 });
